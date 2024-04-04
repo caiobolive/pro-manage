63a50ade6e78035ac7bb5b89cab1d485d8cd2771
caiobolive <caiobolive@gmail.com>
2024-04-04 14:30:06 -0300

    feat: updating data for deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-merito'),
+      dir: require('path').join(__dirname, './coverage/pro-manage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
